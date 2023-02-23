5dc8268c53b33a8b3609333793c19df1a15de2fd
Quantumult X <54837333+oreoleeus@users.noreply.github.com>
2023-02-23 13:26:50 +0800

    Update Net-test.js

--- Surge/Scripts/Net-test.js
+++ Surge/Scripts/Net-test.js
@@ -7,7 +7,7 @@ github:'https://www.github.com'
 }
 
 !(async () => {
-await Promise.all([http($.baidu),http($.bilibili),http($. github),http($. google),http($.youtube)]).then((x)=>{
+await Promise.all([http($.baidu),/*http($.bilibili),*/http($. github),http($. google),http($.youtube)]).then((x)=>{
 	$done({
     title: '网络迟延测试',
     content: x.join('\n'),
